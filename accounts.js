783b5d3d1124c698748d70fef0fd61e65b46bc85
mehrik <sobreo.m@gmail.com>
2016-03-07 11:39:34 -0800

    Add instagram login. Need fix accounts.js modularization

---
+++ accounts.js
@@ -0,0 +1,9 @@
+ServiceConfigration.configurations.remove({
+    service: "instagram"
+});
+ServiceConfiguration.configurations.insert({
+    service: "instagram",
+    clientId: " 748c90721c6a4b9785fa0fde115afff2",
+    scope: "basic",
+    secret: "5947bb9e5c71434abb4a9ed3288afefc"
+});
